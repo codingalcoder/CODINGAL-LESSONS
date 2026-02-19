4c29b1c082aa8ab95cc6a7a7a704eb1b66cb8a48
codingalcoder <dishantiscool236@gmail.com>
2026-02-19 19:08:05 +0530

    lesson 15

---
+++ lesson 15/activity1.py
@@ -0,0 +1,4 @@
+def wellwishes():
+    print("Hello Welcome")
+
+wellwishes()
---
+++ lesson 15/activity3.py
@@ -0,0 +1,24 @@
+def add (P,Q):
+    return P + Q
+def substract (P,Q):
+    return P - Q
+def multiply (P,Q):
+    return P * Q
+def divide (P,Q):
+    return P / Q
+e = input(")Type for your operation for - Add (type a))substract (type b)Multiply (type c)Divide (type d)")
+choice = input("Please enter your choice (a/b/c/d):")
+num_1 = int(input("Please enter the first number :"))
+num_2 = int(input("Please enter the second number :"))
+if choice == 'a':
+    print(num_1, "+" ,num_2,"=",add(num_1, num_2))
+
+elif choice == 'b':
+    print(num_1, "-" ,num_2,"=",substract(num_1, num_2))
+
+elif choice == 'c' :
+    print(num_1, "*" ,num_2,"=",multiply(num_1, num_2))
+elif choice == 'd':
+    print(num_1, "/" ,num_2,"=",divide(num_1, num_2))
+else:
+    print("this is invalid input")
